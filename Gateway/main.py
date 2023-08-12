0c947122af525d9d54d82ee8e9445cc503158d68
Mahdi Erfanian <erfanianmahdi00@gmail.com>
2023-08-11 21:31:22 -0500

    Add public ip

--- Gateway/main.py
+++ Gateway/main.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+origins = ["http://localhost:3000", "http://127.0.0.1:3000", "http://10.8.246.165"]
 
 app.add_middleware(
     CORSMiddleware,
