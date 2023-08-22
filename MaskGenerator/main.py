929d7ad8bb1a21898be8ee99a0a3f161afe6aa5c
Mahdi Erfanian <erfanianmahdi00@gmail.com>
2023-08-22 12:49:54 -0500

    Add accuracy levels to MaskGenerators

--- MaskGenerator/main.py
+++ MaskGenerator/main.py
@@ -9,20 +9,20 @@ import square_remover
 
 app = FastAPI()
 
-class MaskLevel(enum.Enum):
-    ACCURATE = "rembg"
-    MODERATE = "brush"
-    LOOSE = "square"
+class AccuracyLevel(enum.Enum):
+    ACCURATE = "accurate"
+    MODERATE = "moderate"
+    IMPRECISE = "imprecise"
 
 
 @app.post("/v1/images/masks")
-async def generate_mask(image: UploadFile, mask_type: MaskLevel = MaskLevel.ACCURATE):
+async def generate_mask(image: UploadFile, accuracy_level: AccuracyLevel = AccuracyLevel.ACCURATE):
     import invert_remover
     base = remove(image.file.read(), alpha_matting=True)
 
-    if mask_type == MaskLevel.MODERATE:
+    if accuracy_level == AccuracyLevel.MODERATE:
         output = brush_remover.draw_circle_around_transparent_pixels(base)
-    elif mask_type == MaskLevel.LOOSE:
+    elif accuracy_level == AccuracyLevel.IMPRECISE:
         output = square_remover.find_transparent_bounding_box(base)
     else:
         output = base
